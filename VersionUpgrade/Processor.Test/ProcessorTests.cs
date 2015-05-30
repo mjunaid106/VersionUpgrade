a59d9ba12caedc6d106353d98ec653efa0f4e735
Muhammad.Junaid <muhammad.junaid@amorgroup.com>
2015-05-31 00:23:09 +0100

    Added more tests

--- VersionUpgrade/Processor.Test/ProcessorTests.cs
+++ VersionUpgrade/Processor.Test/ProcessorTests.cs
@@ -33,6 +33,13 @@ namespace Processor.Test
         }
 
         [TestMethod]
+        public void Update_WithShortVersion_FullFormat_Space()
+        {
+            var updatedString = _manager.Update("REM v 1.1.0");
+            Assert.AreEqual("REM v 1.1.0", updatedString);
+        }
+
+        [TestMethod]
         public void Update_WithLongVersion_FullFormat_Successful()
         {
             var updatedString = _manager.Update("REM version 1.1.0");
@@ -44,6 +51,34 @@ namespace Processor.Test
         {
             var updatedString = _manager.Update("REM version 1.1");
             Assert.AreEqual("REM version 1.2", updatedString);
+        }
+
+        [TestMethod]
+        public void Update_WithLongVersion_FullFormat_Space()
+        {
+            var updatedString = _manager.Update("REM version1.1.0");
+            Assert.AreEqual("REM version1.1.0", updatedString);
+        }
+
+        [TestMethod]
+        public void Update_WithLongAndShortVersion_LongFormat_Successful()
+        {
+            var updatedString = _manager.Update("REM v1.1.0 version 1.1.0");
+            Assert.AreEqual("REM v1.2.0 version 1.2.0", updatedString);
+        }
+
+        [TestMethod]
+        public void Update_WithLongAndShortVersion_ShortFormat_Successful()
+        {
+            var updatedString = _manager.Update("REM v1.1 version 1.1");
+            Assert.AreEqual("REM v1.2 version 1.2", updatedString);
+        }
+
+        [TestMethod]
+        public void Update_WithLongAndShortVersion_LongAndShortFormat_Successful()
+        {
+            var updatedString = _manager.Update("REM v1.1.0 version 1.1");
+            Assert.AreEqual("REM v1.2.0 version 1.2", updatedString);
         }
     }
 }
