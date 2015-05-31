2df20f5b0f6da39cef64189bed8ea87c261b4c72
Muhammad.Junaid <muhammad.junaid@amorgroup.com>
2015-05-31 21:29:23 +0100

    Added sample files

---
+++ VersionUpgrade/StartupApp/Files/basic files/Basic files/Basic Sub Folder/GoodByeWorld - Copy (3).bas
@@ -0,0 +1,12 @@
+REM this is a basic sample 
+REM v1.1.0
+
+10 CLS
+20 PRINT "Good bye, world.  Thanks for all the fish."
+30 PRINT "I'm learning about commands in BASIC."
+40 PRINT "This text is being printed via the PRINT command."
+50 PRINT "On the next line, I'll use CLS."
+60 CLS   "Now the next word would be PRINT."
+70 PRINT "Finally, on line 80, I'll use END."
+80 END   "And return to PRINT"
+90 PRINT "Now my program is over."
---
+++ VersionUpgrade/StartupApp/Files/cs files/cs files - Copy (13)/File - Copy (12).cs
@@ -0,0 +1,16 @@
+//
+//	Author
+//	Description
+//	Version 1.1.1
+//
+
+Lorem ipsum himenaeos mollis cubilia accumsan et curabitur torquent ante aliquam molestie aptent curae sagittis, aliquam molestie proin etiam condimentum porttitor ligula elit curabitur quis sem convallis.
+
+Convallis id vestibulum hac tristique taciti duis faucibus tempor, fermentum venenatis euismod quisque eu hac eleifend ultricies, aliquam mattis eros tincidunt euismod vivamus dui.
+
+Dictum adipiscing Version 1.1.1 netus erat nostra enim senectus magna sagittis, curabitur ad ullamcorper interdum et ut condimentum lobortis, phasellus lorem a lorem fames consectetur quis vulputate.
+
+Odio elit ac consectetur senectus ligula urna sem ante id, scelerisque sodales commodo duis faucibus at aliquam himenaeos lobortis, eu netus auctor suscipit laoreet gravida sapien condimentum.
+
+Placerat malesuada ac nam lacinia turpis aliquet etiam mattis, hendrerit mattis mollis per et inceptos ullamcorper orci, fusce pellentesque scelerisque congue dolor fermentum bibendum.
+
