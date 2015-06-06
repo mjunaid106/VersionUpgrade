b718ac473292dbf35b884f6bd1ec3e66f8f4f609
Muhammad.Junaid <muhammad.junaid@amorgroup.com>
2015-06-06 18:32:00 +0100

    Updated assembly info

--- VersionUpgrade/StartupApp/Properties/AssemblyInfo.cs
+++ VersionUpgrade/StartupApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("StartupApp")]
+[assembly: AssemblyTitle("VersionUpgrade")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StartupApp")]
+[assembly: AssemblyCompany("Symantec")]
+[assembly: AssemblyProduct("VersionUpgrade")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
